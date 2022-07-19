f833f41e0726a0e695a5290aa5e57bf967e6c552
Yury Anisimau <Yury.Anisimau@coxautoinc.com>
2022-07-19 13:17:49 +0300

    feat: implement dataConferter function

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
+
+import { Table, Filters, Sort, Search } from './components';
+import { getImages, getUsers, getAccounts } from './mocks/api';
+
+import styles from './App.module.scss';
+
+import type { Row } from './components';
+import type { Image, User, Account } from './types';
+
+import rows from './mocks/rows.json';
+import { dataConverter } from './utils/dataConverter';
+
+// mockedData has to be replaced with parsed Promises’ data
+const mockedData: Row[] = rows.data;
+
+function App() {
+	const [data, setData] = useState<Row[]>(undefined);
+
+	useEffect(() => {
+		// fetching data from API
+		Promise.all([getImages(), getUsers(), getAccounts()]).then(
+			([images, users, accounts]: [Image[], User[], Account[]]) => {
+				const newData = dataConverter(images, users, accounts);
+				console.log(newData);
+				setData(newData);
+			}
+		);
+	}, []);
+
+	return (
+		<StyledEngineProvider injectFirst>
+			<div className="App">
+				<div className={styles.container}>
+					<div className={styles.sortFilterContainer}>
+						<Filters />
+						<Sort />
+					</div>
+					<Search />
+				</div>
+				<Table rows={data || mockedData} />
+			</div>
+		</StyledEngineProvider>
+	);
+}
+
+export default App;
---
+++ src/utils/dataConverter.ts
@@ -0,0 +1,40 @@
+import { Row } from '../components';
+import { User, Account, Image, Payment } from '../types';
+
+export interface IDataConverter {
+	users: User[];
+	accounts: Account[];
+	images: Image[];
+}
+
+function compare(a: Payment, b: Payment) {
+	if (new Date(a.date) > new Date(b.date)) {
+		return -1;
+	}
+	if (new Date(a.date) < new Date(b.date)) {
+		return 1;
+	}
+	return 0;
+}
+
+export const dataConverter = (
+	images: Image[],
+	users: User[],
+	accounts: Account[]
+): Row[] => {
+	return users.reduce((acc, user) => {
+		const image = images.find((image) => image.userID === user.userID);
+		const account = accounts.find((account) => account.userID === user.userID);
+		const lastPayments = account.payments.sort(compare)[0];
+
+		acc.push({
+			avatar: image ? image.url : '',
+			username: user.username,
+			country: user.country,
+			name: user.name,
+			lastPayments: lastPayments ? lastPayments.totalSum : 0,
+			posts: account ? account.posts : 0,
+		});
+		return acc;
+	}, []);
+};
